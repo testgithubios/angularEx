3498663ec4c2779f3997310edcb3c5058dbc9e71
test28989 <test28989@gmail.com>
2018-01-08 21:10:57 +0700

    day 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,9 +13,20 @@ export class HeroesComponent implements OnInit {
 	    id: 1,
 	    name: 'Windstorm'
 	  };
-  constructor() { }
+	  heroes = [];
+	  selectedHero: Hero;
+	  
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+      this.getHeroes();
   }
-
+  
+  onSelect( hero: Hero ): void {
+      this.selectedHero = hero;
+  }
+  getHeroes(): void {
+      this.heroService.getHeroes()
+          .subscribe(heroes => this.heroes = heroes);
+  }
 }
